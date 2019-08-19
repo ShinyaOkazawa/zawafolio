316d5350492d111ab15ee500a99edfb1a66696ee
ShinyaOkazawa <fess3idm@gmail.com>
2019-08-19 13:49:42 +0900

    add favorites section

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <PageContainer>
     <IndexHero />
+    <IndexFavorites />
   </PageContainer>
 </template>
 
 <script>
 import PageContainer from '~/components/common/PageContainer'
 import IndexHero from '~/components/index/IndexHero'
+import IndexFavorites from '~/components/index/IndexFavorites'
 export default {
   components: {
     PageContainer,
-    IndexHero
+    IndexHero,
+    IndexFavorites
   },
   created() {
     // console.log(this.$route)
