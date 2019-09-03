f3b1eca6026ef5b2c90f473a48b084defecbfdbc
ShinyaOkazawa <fess3idm@gmail.com>
2019-09-03 23:18:04 +0900

    add background-image for container

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="container">
-    <TheGlobalNav />
-    <nuxt />
-    <TheGlobalHeader />
+  <div class="container" :style="`background-image:url(${containerBackgroundImage})`">
+    <div class="container-inner">
+      <TheGlobalNav />
+      <nuxt />
+      <TheGlobalHeader />
+    </div>
   </div>
 </template>
 
 <script>
 import TheGlobalNav from '~/components/common/modules/TheGlobalNav'
 import TheGlobalHeader from '~/components/common/modules/TheGlobalHeader'
+import containerBackgroundImage from '~/assets/images/bg-repeat-zawa.png'
+
 export default {
+  data() {
+    return {
+      containerBackgroundImage: containerBackgroundImage
+    }
+  },
   components: {
     TheGlobalNav,
     TheGlobalHeader
@@ -19,6 +28,9 @@ export default {
 
 <style scoped lang="scss">
 .container {
+  background-size: 150px auto;
+}
+.container-inner {
   display: flex;
   max-width: 1024px;
   height: 100vh;
